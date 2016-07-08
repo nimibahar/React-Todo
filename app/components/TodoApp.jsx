d1d37ea646e00fbffde82a0277bf96e38ffb19df
Nimrod Bahar <nimrodbahar@gmail.com>
2016-07-08 11:55:15 +0200

    Add test suit for list component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,32 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        }, {
+          id: 2,
+          text: 'Wash the dishes'
+        }, {
+          id: 3,
+          text: 'Install dishwasher'
+        }, {
+          id: 4,
+          text: 'Install washing machine'
+        }
+      ]
+    }
+  },
   render: function () {
+    var {todos} = this.state;
+
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
