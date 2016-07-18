ad61026cdeda63de02bff2e5cf2f37d0d4cdb554
Nimrod Bahar <nimrodbahar@gmail.com>
2016-07-18 16:14:26 +0200

    Add actions and action tests

---
+++ app/actions/actions.jsx
@@ -0,0 +1,31 @@
+export var setSearchText = (searchText) => {
+  return {
+    type: 'SET_SEARCH_TEXT',
+    searchText
+  };
+};
+
+//toggleShowCompleted TOGGLE_SHOW_COMPLETED
+
+
+export var toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED'
+  };
+};
+
+export var addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  };
+};
+
+ // toggleTodo(id) TOGGLE_TODO
+
+ export var toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  };
+ };
