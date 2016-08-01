d5ce974ffc1eb9c59e17ccabe85b94c4c75606f3
Nimrod Bahar <nimrodbahar@gmail.com>
2016-08-01 13:07:26 +0200

    Move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,10 +2,10 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyCd5SWrDbWl1Jx26NKDqjkOTJxVzn0TWCo",
-    authDomain: "nimi-todo-app.firebaseapp.com",
-    databaseURL: "https://nimi-todo-app.firebaseio.com",
-    storageBucket: "nimi-todo-app.appspot.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
   };
   firebase.initializeApp(config);
 } catch (e) {
